1bec0dfdc3f8de4e05befbaa649e9153947bff81
haleycrowder <haleycrowder@vt.edu>
2024-02-02 13:41:25 -0500

    filtering but added comments in

--- Filtering.R
+++ Filtering.R
@@ -1,11 +1,14 @@
 library(dplyr)
 library(tidyverse)
 
+#combines the two air temperatures into one and deletes the exta column
 allairtemps <- bind_rows(HBEF_STA1airTemp_15min, HBEF_STA23airTemp_15min)
 allairtemps <- select(allairtemps,-FLAG)
 
+#filters the air temperatures hourly
 allairtemps <- allairtemps %>%
   filter(format(DateTime, "%M") == "00")
 
+#filters the stream temperatures hourly
 streamtemps <- HBEF_streamtemp_roughlyCleaned %>%
   filter(format(TIMESTAMP, "%M") == "00")
