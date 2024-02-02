bcddbf5d82e1c0c35e1aeb5ed4bbd7232970eda2
haleycrowder <haleycrowder@vt.edu>
2024-02-02 14:40:31 -0500

    rough draft

--- Filtering.R
+++ Filtering.R
@@ -6,7 +6,7 @@ STA1Air <- read_csv("HBEF_STA1airTemp_15min.csv")
 STA23Air <- read_csv("HBEF_STA23AirTemp_15min.csv")
 StreamTemp <- read_csv("HBEF_streamtemp_roughlyCleaned.csv")
 
-#combines the two air temperatures into one and deletes the exta column
+#combines the two air temperatures into one and deletes the extra column
 allairtemps <- bind_rows(STA1Air, STA23Air)
 allairtemps <- select(allairtemps,-FLAG)
 
@@ -17,3 +17,21 @@ allairtemps <- allairtemps %>%
 #filters the stream temperatures hourly
 streamtemps <- StreamTemp %>%
   filter(format(TIMESTAMP, "%M") == "00")
+
+tempscombined <- streamtemps %>%
+  pivot_longer(cols = starts_with("Streamtemp"),
+               names_to = 'Location') %>%
+  mutate(streamtemps = case_when(
+    Location = Streamtemp_W1 ~ STA_1,
+    Location = Streamtemp_W2 ~ STA_1,
+    Location = Streamtemp_W3 ~ STA_1,
+    Location = Streamtemp_W4 ~ STA_1,
+    Location = Streamtemp_W5 ~ STA_1,
+    Location = Streamtemp_W6 ~ STA_1,
+    Location = Streamtemp_W7 ~ STA_23,
+    Location = Streamtemp_W8 ~ STA_23,
+    Location = Streamtemp_W9 ~ STA_23
+ )) %>%
+  join_by(TIMESTAMP == DateTime)
+
+    
